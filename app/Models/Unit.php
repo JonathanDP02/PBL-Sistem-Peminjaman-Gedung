66bc62010f96c85b82a8a28f6b860ab79ec9e1d6
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-03-29 17:57:38 +0700

    fix: Update Unit model primary key and key type for consistency

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -10,7 +10,7 @@ class Unit extends Model
     /** @use HasFactory<\Database\Factories\UnitFactory> */
     use HasFactory;
 
-    protected $primaryKey = 'unit_id';
-    public $incrementing = false;
-    protected $keyType = 'string';
+    protected $primaryKey = 'role_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
 }
