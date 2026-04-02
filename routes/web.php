1d80a12126375f7e63069297729364b717bd6646
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-04-02 17:13:33 +0700

    feat: Add profile routes with authentication middleware for user profile management

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,27 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/login', function () {
-    return view('login'); 
-})->name('login');//
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+//siapapun yang masuk , aslinya ada di dasboard saja ini
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cari-ruangan', function () {
+        return view('cari-ruangan');
+    })->name('cari-ruangan');
+    
+    Route::get('/jadwal-saya', function () {
+        return view('user.jadwal-saya');
+    })->name('jadwal-saya');
+    
+    Route::get('/riwayat', function () {
+        return view('riwayat');
+    })->name('riwayat');
+});
 
 // Fungsi prefix('kata') di Laravel digunakan untuk menambahkan "kata" tersebut di bagian paling depan dari semua URL yang ada di dalam grup tersebut.
 Route::middleware(['auth', 'checkRole:SuperAdmin,AdminUnit'])->prefix('admin')->group(function () {
@@ -30,3 +41,12 @@ Route::middleware(['auth', 'checkRole:User'])->prefix('user')->group(function ()
         return view('user.peminjaman');
     })->name('peminjaman'); //nama dari route ini nanti dipakai di AuthController untuk redirect setelah login sesuai role
 });
+
+// ini route untuk halaman profile, hanya bisa diakses kalau sudah login (middleware auth)
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
